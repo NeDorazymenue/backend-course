6f66f477afa1f6bd4377bd9285930a84129ef944
nedorazymenue <dfer4517@gmail.com>
2024-12-03 14:06:46 +0300

    Изменение в базе данных -> email unique

--- src/api/auth.py
+++ src/api/auth.py
@@ -32,9 +32,6 @@ async def register_user(data: UsersRequestAdd = Body(openapi_examples={
     hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        existing_user = await UsersRepository(session).get_one_or_none(email=new_user_data.email)
-        if existing_user:
-            raise HTTPException(status_code=400, detail="User with this email already exists.")
         await UsersRepository(session).add(new_user_data)
         await session.commit()
     return {"status": "OK"}
