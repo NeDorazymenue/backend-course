13b10e60694906df96218e320ed0cc0d9ff8cdbf
nedorazymenue <dfer4517@gmail.com>
2025-03-06 22:22:02 +0300

    улучшенное решение

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -14,18 +14,31 @@ async def test_booking_crud(db):
         date_to=date(2025, 8, 18),
         price=100,
     )
-    booking = await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
-    booking_data_in_db = (await db.bookings.get_filtered(id=booking.id))[0]
-    assert booking_data_in_db==booking
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
 
-    booking_data.price = 300
-    await db.bookings.edit(data=booking_data, exclude_unset=True, id=booking.id)
-    update_booking_data = (await db.bookings.get_filtered(id=booking.id))[0]
-    assert update_booking_data.price == booking_data.price
 
-    await db.bookings.delete(id=booking.id)
-    deleted_booking = await db.bookings.get_filtered(id=booking.id)
-    assert deleted_booking==[]
+    update_price = 300
+    update_booking_data = BookingAdd(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=date(2025, 8, 8),
+        date_to=date(2025, 8, 18),
+        price=update_price,
+    )
+    await db.bookings.edit(update_booking_data, id=new_booking.id)
+    update_booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert update_booking
+    assert update_booking.id == new_booking.id
+    assert update_booking.price == update_price
+
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
 
     await db.commit()
